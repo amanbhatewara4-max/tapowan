04f727fc839c8f73c1b42f4f40f83af349c4760c
amanbhatewara4-max <amanbhatewara4@gmail.com>
2025-11-28 11:11:34 +0530

    Add files via upload

---
+++ tree at tapovan area/py code.py
@@ -0,0 +1,9 @@
+boundary_layer_name = 'blocks_boundary'
+trees_layer_name = 'trees'
+grid_size = 3.0
+clearance = 1.0
+boundary_layer_name = 'MYBLOCK'
+trees_layer_name = 'TREEPOINTS'
+
+output_shapefile = None
+output_shapefile = r'C:\Users\Amy\Desktop\grid3m_clean.shp'
